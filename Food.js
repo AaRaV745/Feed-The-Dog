63beebd70bba4f31a0549ba1f34b8d604bfe2efc
AaRaV745 <81541820+AaRaV745@users.noreply.github.com>
2021-07-19 15:12:55 -0400

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,48 @@
+class Food{
+    constructor(){
+        this.foodstock = 0;
+        this.image = loadImage("images/Milk.png");
+        this.lastFed;
+    }
+
+    updateFoodStock(foodstock){
+        this.foodstock = foodstock
+    }
+
+    getFedTime(lastFed) {
+        this.lastFed = lastFed;
+    }
+
+    removeFood(){
+        if(this.foodstock>0){
+
+            this.foodstock = this.foodstock - 1;
+        }
+    }
+
+    getFoodStock(){
+        return this.foodstock;
+    }
+
+    display(){
+        var x = 80;
+        var y = 100;
+        
+        imageMode(CENTER);
+        image(this.image,720,220,70,70);
+
+        if(this.foodstock != 0){
+            for(var i = 0; i<this.foodstock; i++){
+                if(1%10==0){
+                    x = 80;
+                    y=y+50;
+                }
+                image(this.image,x,y, 50,50);
+                x=x+30
+        }
+
+    }
+
+ }
+
+}
